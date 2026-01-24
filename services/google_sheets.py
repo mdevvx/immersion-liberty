14c7676f0e68e0d9ae1a7d29f0f9ece2631469e4
Muhammad Nadeem <muhammadnadeem.1596@gmail.com>
2026-01-24 20:15:57 +0500

    google sheet file changed

--- services/google_sheets.py
+++ services/google_sheets.py
@@ -142,7 +142,8 @@
 
 #     print("Row appended successfully")
 
-
+import os
+import json
 from pathlib import Path
 import gspread
 from google.oauth2.service_account import Credentials
@@ -150,11 +151,15 @@ from datetime import datetime
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-CREDS_FILE = BASE_DIR / "config" / "google_service_account.json"
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# CREDS_FILE = BASE_DIR / "config" / "google_service_account.json"
 
-if not CREDS_FILE.exists():
-    raise FileNotFoundError(f"Google creds not found at {CREDS_FILE}")
+# if not CREDS_FILE.exists():
+#     raise FileNotFoundError(f"Google creds not found at {CREDS_FILE}")
+
+CREDS_JSON = os.getenv("GOOGLE_CREDS_JSON")
+if not CREDS_JSON:
+    raise RuntimeError("GOOGLE_CREDS_JSON is not set")
 
 SPREADSHEET_ID = "1pT2jbO4BjNMLXM6QiQ_WYgUAPuQhqpm48ghFtOKQufM"
 SHEET_NAME = "Verifications"
@@ -170,10 +175,13 @@ HEADERS = [
 
 
 def get_sheet():
-    creds = Credentials.from_service_account_file(
-        CREDS_FILE,
-        scopes=SCOPES,
-    )
+    # creds = Credentials.from_service_account_file(
+    #     CREDS_FILE,
+    #     scopes=SCOPES,
+    # )
+    creds_info = json.loads(CREDS_JSON)
+    creds = Credentials.from_service_account_info(creds_info, scopes=SCOPES)
+    # client = gspread.authorize(creds)
     client = gspread.authorize(creds)
     return client.open_by_key(SPREADSHEET_ID).worksheet(SHEET_NAME)
 
